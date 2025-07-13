823a8dd09bddcbc42db393ccd263c88c590270c4
kwangjin <isimilej@gmail.com>
2025-07-13 19:36:03 +0900

    [Co-1] 폰트 추가

--- app/src/main/java/com/isimilej/corns/ui/theme/Type.kt
+++ app/src/main/java/com/isimilej/corns/ui/theme/Type.kt
@@ -2,14 +2,36 @@ package com.isimilej.corns.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.isimilej.corns.R
+
+val pretendard = FontFamily(
+    Font(
+        R.font.pretendard_bold,
+        FontWeight.Bold,
+        FontStyle.Normal
+    ),
+    Font(
+        R.font.pretendard_semibold,
+        FontWeight.SemiBold,
+        FontStyle.Normal
+    ),
+    Font(
+        R.font.pretendard_regular,
+        FontWeight.Normal,
+        FontStyle.Normal
+    ),
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = pretendard,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
